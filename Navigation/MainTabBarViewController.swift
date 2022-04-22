cfa0a26e2013c3ded6a8c6777232be28b23fec07
Kirill Feev <kirill.feev@mail.ru>
2022-04-22 10:43:53 +0300

    clear comment

--- Navigation/MainTabBarViewController.swift
+++ Navigation/MainTabBarViewController.swift
@@ -11,7 +11,6 @@ class MainTabBarViewController: UITabBarController {
 
     let feedVc = FeedViewController()
     let profileVc = ProfileViewController()
-//    let postVc = PostViewController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,24 +18,14 @@ class MainTabBarViewController: UITabBarController {
     }
 
     private func setupControllers() {
-//        firstVC.tabBarItem.title = "First"
-//        firstVC.tabBarItem.image = UIImage(systemName: "bolt")
-//        secondVC.tabBarItem.title = "Second"
-//        secondVC.tabBarItem.image = UIImage(systemName: "bolt")
-//        postVc.tabBarItem.title = "Новый пост"
         
         let feedNavController = UINavigationController(rootViewController: feedVc)
         feedNavController.tabBarItem.title = "Лента"
         feedNavController.tabBarItem.image = UIImage(systemName: "bolt")
-//        feedNavController.addChild(postVc)
         
         let profileNavController = UINavigationController(rootViewController: profileVc)
         profileNavController.tabBarItem.title = "Профиль пользователя"
         profileNavController.tabBarItem.image = UIImage(systemName: "face.smiling")
-        
-//        let postNavController = UINavigationController(rootViewController: postVc)
-//        postNavController.tabBarItem.title = "Пост"
-//        postNavController.tabBarItem.image = UIImage(systemName: "face.smiling")
         
         viewControllers = [feedNavController, profileNavController]
     }
