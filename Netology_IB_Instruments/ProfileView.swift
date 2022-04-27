8564444ae9ee19e584ce2b069c3ee84e839f594e
Kirill Feev <kirill.feev@mail.ru>
2022-04-27 20:30:48 +0300

    add home work

---
+++ Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,22 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Кирилл on 22.04.2022.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    @IBOutlet weak var avatarImage: UIImageView!
+    
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    @IBOutlet weak var birthDateLabel: UILabel!
+    
+    @IBOutlet weak var sityLabel: UILabel!
+    
+    @IBOutlet weak var userTextView: UITextView!
+    
+}
